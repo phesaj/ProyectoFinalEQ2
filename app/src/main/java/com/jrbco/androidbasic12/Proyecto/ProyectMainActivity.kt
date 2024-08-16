0b7bc19ef65e7d61e44b160410dca9d9fd6d0e2b
juan <juanjosegonzalesmelgarejo@gmail.com>
2024-08-16 02:17:59 -0600

    arreglando cosas

--- app/src/main/java/com/jrbco/androidbasic12/Proyecto/ProyectMainActivity.kt
+++ app/src/main/java/com/jrbco/androidbasic12/Proyecto/ProyectMainActivity.kt
@@ -6,14 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.jrbco.androidbasic12.R
+import com.jrbco.androidbasic12.databinding.ActivityFragmentBinding
+import com.jrbco.androidbasic12.databinding.ActivityProyectMainBinding
+import com.jrbco.androidbasic12.ejercicioclase.ejercicio2.fragment.FirstViewbindingFragment
 
 class ProyectMainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityProyectMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_proyect_main)
 
+        binding = ActivityProyectMainBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment, DisplayInfoFragment.newInstance())
+            .addToBackStack("DisplayInfoFragment")
+            .commit()
 
 
     }
